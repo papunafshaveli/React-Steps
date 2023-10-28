fab3d2b7d89170ad04f51085699a4b962493ed1f
Papuna Fshaveli <papunafshaveli@gmail.com>
2023-10-28 11:58:40 +0400

    add close button with its functionals

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const messages = [
 ];
 function App() {
   const [step, setStep] = useState(1);
-
+  const [isOpen, setIsOpen] = useState(true);
   const handleNxet = () => {
     if (step < 3) setStep(step + 1);
   };
@@ -18,30 +18,42 @@ function App() {
   };
 
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 1 ? "active" : ""}>1</div>
-        <div className={step >= 2 ? "active" : ""}>2</div>
-        <div className={step >= 3 ? "active" : ""}>3</div>
-      </div>
-      <p className="message">
-        step {step}: {messages[step - 1]}
-      </p>
-      <div className="buttons">
-        <button
-          style={{ backgroundColor: "#7950f2", color: "white" }}
-          onClick={handlePrevious}
-        >
-          Previous
-        </button>
-        <button
-          style={{ backgroundColor: "#7950f2", color: "white" }}
-          onClick={handleNxet}
-        >
-          Next
-        </button>
-      </div>
-    </div>
+    <>
+      <button
+        className="close"
+        onClick={() => {
+          setIsOpen(!isOpen);
+        }}
+      >
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+          <p className="message">
+            step {step}: {messages[step - 1]}
+          </p>
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "white" }}
+              onClick={handlePrevious}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "white" }}
+              onClick={handleNxet}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
